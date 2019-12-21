4e3af40ca77edc50acc0dcc40af50dc1e15e8dda
张晃 <mr.lovable@192.168.124.8>
2019-12-21 20:03:02 +0800

    Updates

---
+++ precache-manifest.33574aef2a63dce50f41fada55fdc515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7812dbfc5e6e5b70a88d1d94a9ea9c94",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9e8b420e95e3295305b9",
+    "url": "./static/css/main.39a00a87.chunk.css"
+  },
+  {
+    "revision": "c962cbff5bc65b18afdf",
+    "url": "./static/js/2.d8ee8a7d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.d8ee8a7d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e8b420e95e3295305b9",
+    "url": "./static/js/main.5418a486.chunk.js"
+  },
+  {
+    "revision": "e86ef444b9dfb39ddafd",
+    "url": "./static/js/runtime-main.c3c71c64.js"
+  }
+]);
